d2170d532897a14439a1fab7c1e17f05cbd9cee9
lexa <sokolov@ozero.net>
2017-11-08 17:40:35 +0300

    Initialized browser with JavaScript and added permission for internet

--- app/src/main/java/net/ozero/browser/MainActivity.java
+++ app/src/main/java/net/ozero/browser/MainActivity.java
@@ -2,12 +2,21 @@ package net.ozero.browser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.google.ru/");
+
+
     }
 }
